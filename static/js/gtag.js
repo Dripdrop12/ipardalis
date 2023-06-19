083b5ccc3e0a1beddc1b48f6b987678d9b46ce9f
Jonathan Hill <jon.mark.hill@ipardalis.com>
2023-06-18 23:29:36 -0400

    update

--- static/js/gtag.js
+++ static/js/gtag.js
@@ -1,5 +1,6 @@
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-8KLZ4396EQ"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-8KLZ4396EQ">
+</script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
