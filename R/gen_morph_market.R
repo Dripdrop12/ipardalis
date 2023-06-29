2ca23ec91a72ae8e837e7024e3e62708a2d70cc7
Jonathan Hill <jon.mark.hill@ipardalis.com>
2023-06-29 02:48:39 -0400

    add new morph market content

---
+++ R/gen_morph_market.R
@@ -0,0 +1,63 @@
+require(fs)
+require(yaml)
+require(dplyr)
+require(tidyr)
+require(lubridate)
+require(stringr)
+
+gen_morph_market <- function(
+    clutch_dir = "data/clutches/", 
+    file_name = "R/test.csv", 
+    sire_list = c("jackson", "manjaka", "ralph", "tony", "zandrin", "zava", "zozoro")){
+  
+  clutch_list <- dir_map(clutch_dir, read_yaml, type = "file")
+  clutch_df <- as.data.frame(do.call(rbind, clutch_list))
+  
+  clutch_df <- clutch_df %>%
+    filter(
+      !map_vec(soldoutmale, is.null),
+      map_vec(soldoutmale, is.list),
+      sire %in% sire_list
+    ) %>%
+    unnest_longer(soldoutmale) %>%
+    unnest_wider(soldoutmale, names_sep = "-") %>% 
+    mutate_if(is.list, ~ unlist(.)) %>%
+    filter(
+      is.na(`soldoutmale-sold`)) %>%
+    mutate(across(c(laid, hatchstart, hatchend), ymd)) %>%
+    filter(!is.na(hatchend)) %>%
+    mutate(
+      `soldoutmale-phenotype` = if_else(
+        condition = is.na(`soldoutmale-phenotype`),
+        true = "YBBB Ambilobe",
+        false = `soldoutmale-phenotype`
+      ),
+      age_months = interval(hatchend, today()) %/% months(1),
+      Category = "Panther Chameleons",
+      Group_Id = paste0(sire, "-", dam, hatchend),
+      Animal_Id = paste0(`soldoutmale-name`, "-", Group_Id),
+      Title = paste0(
+        age_months, " Month-old ",
+        `soldoutmale-phenotype`, " Panther Chameleon"
+      ),
+      Price = `soldoutmale-price`,
+      Sex = `soldoutmale-gender`,
+      Dob = format(hatchend, "%m-%d-%Y"),
+      Maturity = case_when(
+        age_months < 5 ~ "juvenile",
+        age_months < 9 ~ "Subadult",
+        .default = "Adult"
+      ),
+      Traits = str_replace(`soldoutmale-phenotype`, "Rainbow", "Classic"),
+      Desc = paste0(Title, " (", Animal_Id, ") - ", desc, ifelse(is.na(`soldoutmale-desc`), "", `soldoutmale-desc`)),
+      Origin = "Self Produced",
+      Prey_State = "Live",
+      Prey_Food = "Cricket",
+      Min_Shipping = 40,
+      Max_Shipping = 100,
+      Is_Negotiable = "Will Consider"
+    ) %>%
+    select(Category:Is_Negotiable)
+  
+  write.csv(clutch_df, file_name, row.names = FALSE)
+}
--- content/tos.md
+++ content/tos.md
@@ -10,13 +10,13 @@ By purchasing from iPardalis, you are agreeing to our terms of service.
 
 **We require pictures of your enclosure pre-shipment**
 
-We ship Monday, Tuesday or Wednesday via FedEx Priority Overnight for next day arrival. iPardalis has live arrival gaurantees on all shipments.
+We ship Monday, Tuesday or Wednesday via FedEx Priority Overnight for next day arrival. iPardalis has live arrival guarantees on all shipments.
 
 FedEx requires a phone # for shipping live animals in case they need to reach out to the recipient directly. We will also need to look up the closest shipping center to your zip code and confirm prior to shipping.
 
-## Quality Gauranteed
+## Quality Guaranteed
 
-We gaurantee the quality of our animals. On top of our live arrival gaurantee, if you are unsatisfied for any reason, you can request a 100% refund/replacement upon safe return to iPardalis. We will want to understand your reasoning, so please be prepared to discuss and share pictures. If it is determined that improper care resulted in injury, poor health or death, then this gaurantee is null and void. Please note, you will be responsible for shipping in the case of a replacement/refund.
+We guarantee the quality of our animals. On top of our live arrival guarantee, if you are unsatisfied for any reason, you can request a 100% refund/replacement upon safe return to iPardalis. We will want to understand your reasoning, so please be prepared to discuss and share pictures. If it is determined that improper care resulted in injury, poor health or death, then this guarantee is null and void. Please note, you will be responsible for shipping in the case of a replacement/refund.
 
 ## Deposits
 
