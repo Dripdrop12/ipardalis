84fbc10af856f2563271b5e94244ecf7fc70a109
Jonathan Hill <jon.mark.hill@ipardalis.com>
2023-09-08 15:17:31 -0400

    update mm gen

--- R/gen_morph_market.R
+++ R/gen_morph_market.R
@@ -9,53 +9,43 @@ require(purrr)
 gen_morph_market <- function(
     clutch_dir = "data/clutches/", 
     file_name = "R/morphmarket.csv",
-    morph_market_export = "~/Downloads/animals(3).csv",
-    sire_list = c("jackson", "manjaka", "ralph", "tony", "zandrin", "zava", "zozoro")){
+    morph_market_export = "~/Downloads/animals(3).csv"){
   
   clutch_list <- dir_map(clutch_dir, read_yaml, type = "file")
-  clutch_df <- as.data.frame(do.call(rbind, clutch_list))
+  clutch_df <- as.data.frame(do.call(bind_rows, clutch_list))
   
   clutch_df <- clutch_df %>%
-    filter(
-      !map_vec(soldoutmale, is.null),
-      map_vec(soldoutmale, is.list),
-      sire %in% sire_list
-    ) %>%
-    unnest_longer(soldoutmale) %>%
-    unnest_wider(soldoutmale, names_sep = "-") %>% 
-    mutate_if(is.list, ~ unlist(.)) %>%
-    filter(
-      is.na(`soldoutmale-sold`)) %>%
+    filter(listed == TRUE) %>%
+    unnest_wider(babies, names_sep = "-") %>%
     mutate(across(c(laid, hatchstart, hatchend), ymd)) %>%
-    filter(!is.na(hatchend)) %>%
     mutate(
-      `soldoutmale-phenotype` = if_else(
-        condition = is.na(`soldoutmale-phenotype`),
+      `babies-phenotype` = if_else(
+        condition = is.na(`babies-phenotype`),
         true = "YBBB Ambilobe",
-        false = `soldoutmale-phenotype`
+        false = `babies-phenotype`
       ),
       age_months = interval(hatchend, today()) %/% months(1),
       Category = "Panther Chameleons",
       Group_Id = paste0(sire, "-", dam, hatchend),
-      Animal_Id = paste0(`soldoutmale-name`, "-", Group_Id),
+      Animal_Id = paste0(`babies-name`, "-", Group_Id),
       Title = paste0(
         age_months, " Month-old ",
-        `soldoutmale-phenotype`, " Panther Chameleon"
+        `babies-phenotype`, " Panther Chameleon"
       ),
-      Price = `soldoutmale-price`,
+      Price = `babies-price`,
       State = "For Sale",
       Visibility = "Public",
       Enabled = "Active",
       Is_Group = "No",
-      Sex = `soldoutmale-gender`,
+      Sex = `babies-gender`,
       Dob = format(hatchend, "%m-%d-%Y"),
       Maturity = case_when(
         age_months > 9 ~ "Adult",
         age_months > 5 ~ "Subadult",
         age_months >= 3 ~ "Juvenile"
       ),
-      Traits = str_replace(`soldoutmale-phenotype`, "Rainbow", "Classic"),
-      Desc = paste0(Title, " (", Animal_Id, ") - ", desc, ifelse(is.na(`soldoutmale-desc`), "", `soldoutmale-desc`)),
+      Traits = str_replace(`babies-phenotype`, "Rainbow", "Classic"),
+      Desc = paste0(Title, " (", Animal_Id, ") - ", desc, ifelse(is.na(`babies-desc`), "", `babies-desc`)),
       Origin = "Self Produced",
       Proven_Breeder = "No",
       Quantity = 1,
@@ -66,7 +56,7 @@ gen_morph_market <- function(
       Is_Negotiable = "Will Consider",
       Is_Rep_Photo = "No",
       Is_For_Trade = "No",
-      Photo_Urls = paste0("https://ipardalis.com", `soldoutmale-image`, ".jpg")
+      Photo_Urls = paste0("https://ipardalis.com", `babies-image`, ".jpg")
     ) %>%
     select(Category:Photo_Urls)
   
