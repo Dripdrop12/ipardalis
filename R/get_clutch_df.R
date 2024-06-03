3b9c046c495551b698400e6c76b7de5fba1580c6
Jonathan Hill <jon.mark.hill@ipardalis.com>
2024-06-03 15:15:30 -0400

    fix canonical

--- R/get_clutch_df.R
+++ R/get_clutch_df.R
@@ -73,15 +73,17 @@ get_raw_clutch_df <- function(clutch_dir, listed) {
   clutch_df
 }
 
-fix_listings <- function(listing_dir="content/panther-chameleons-for-sale"){
+fix_listings <- function(new_canonical="/panther-chameleons-for-sale/zozoro/artilly/2023-08-03/m4/", listing_dir="content/panther-chameleons-for-sale"){
   for (file in dir_ls(listing_dir, recurse = T, type = "file")) {
     txt <- read_lines(file)
     sold_loc <- str_which(txt, "baby_sold")
     priority_loc <- str_which(txt, "priority")
+    canon_loc <- str_which(txt, "canonical_link")
     
     if (any(str_detect(txt, "baby_sold")) && str_detect(txt[sold_loc], regex("true", ignore_case = T))) {
       print(paste(file, "is updated"))
       txt[priority_loc] <- "  priority: 0.0"
+      txt[canon_loc] <- paste0("canonical_link: ", new_canonical)
       write_lines(txt, file)
     } 
   }
